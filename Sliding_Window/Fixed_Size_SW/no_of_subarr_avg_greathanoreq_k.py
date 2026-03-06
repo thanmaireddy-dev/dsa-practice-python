139cfcc1ff43a30df354061d2c975ac05e083f4f
thanmai <thanmaireddy77@gmail.com>
2026-03-06 17:36:51 +0530

    number of subarrays with size k and average greater than or equal to threshold

--- Sliding_Window/Fixed_Size_SW/no_of_subarr_avg_greathanoreq_k.py
+++ Sliding_Window/Fixed_Size_SW/no_of_subarr_avg_greathanoreq_k.py
@@ -1,1 +1,20 @@
-def number_of_subarrays_of_size_k_and_average_greatthanore
+def number_of_subarrays_of_size_k_and_average_GorEto_threshold(arr, k, threshold):
+    n= len(arr)
+    left=0
+    count=0
+    winsum= sum(arr[:k])
+    avg= winsum/k
+    if avg>= threshold:
+        count= count+1
+    
+    for right in range(k,n):
+        winsum= winsum+ arr[right]- arr[left]
+        left= left+1
+        avg= winsum/k
+        if winsum>= threshold:
+            count= count+1
+    return count
+        
+print(number_of_subarrays_of_size_k_and_average_GorEto_threshold([2,2,2,2,5,5,5,8],3,4))   
+        
+    
