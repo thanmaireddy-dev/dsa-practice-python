10459839b6677b3308bed015da88c42dafd703a5
thanmai <thanmaireddy77@gmail.com>
2026-03-14 22:27:12 +0530

    longest_substring_with_atmost_two_distinct_characters

--- Sliding_Window/Variable_Size_SW/atmost_two_distinct_chars.py
+++ Sliding_Window/Variable_Size_SW/atmost_two_distinct_chars.py
@@ -1,3 +1,22 @@
 def longest_substring_with_atmost_two_distinct_characters(s):
     n= len(s)
+    p1=p2=0
+    maxlen=0
+    charset= {}
+    for p1 in range(n):
+        if s[p1] in charset:
+            charset[s[p1]]+=1
+        else:
+            charset[s[p1]]=1
+            
+        while len(charset)>2:
+            charset[s[p2]]-=1
+            if charset[s[p2]]==0:
+                del charset[s[p2]]
+            p2= p2+1
+        maxlen= max(maxlen, p1-p2+1)
+        
+    return maxlen
+
+print(longest_substring_with_atmost_two_distinct_characters("ninninja"))
     
---
+++ Sliding_Window/Variable_Size_SW/longest_repeating.py
@@ -0,0 +1,25 @@
+def longest_repeating_character_replacement(s,k):
+    n= len(s)
+    maxlen=0
+    maxfreq=0
+    left=0
+    charset=0
+    for right in range(n):
+        if s[right] in charset:
+            charset[s[right]]+=1
+        else:
+            charset[s[right]]=1
+        maxfreq= max(maxfreq, charset[s[right]])
+        while ((right-left+1)-maxfreq)>k:
+            charset[s[left]]-=1
+            if charset[s[left]]==0:
+                del charset[s[left]]
+            left= left+1
+        maxlen= max(maxlen, right-left+1)
+    return maxlen
+
+print(longest_repeating_character_replacement("AABABBA",1))
+            
+            
+    
+    
