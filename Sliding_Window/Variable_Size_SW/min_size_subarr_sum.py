4f3b3dce001b48723e26079acd70d16fdf9b18ed
thanmai <thanmaireddy77@gmail.com>
2026-03-14 21:49:39 +0530

    minimum size subarray sum problem

---
+++ Sliding_Window/Variable_Size_SW/min_size_subarr_sum.py
@@ -0,0 +1,17 @@
+def minimum_size_subarray_sum(nums, target):
+    n= len(nums)
+    minlen=float('inf')
+    left=0
+    summ=0
+    if sum(nums)<target:
+        return 0
+    for right in range(n):
+        summ= summ+ nums[right]
+        while summ>= target:
+            summ= summ- nums[left]
+            minlen= min(minlen, right-left+1)
+            left= left+1
+    return minlen
+
+print(minimum_size_subarray_sum([1,4,4],4))
+        
