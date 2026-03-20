f3dc036fc6054acf150cef8ea098b14bc5d41b8f
thanmai <thanmaireddy77@gmail.com>
2026-03-20 13:01:31 +0530

    valid palindrome

---
+++ Two_Pointers/valid_palindrome.py
@@ -0,0 +1,17 @@
+def valid_plaindrome(s):
+    n= len(s)
+    p1=0
+    p2=n-1
+    while (p1<p2):
+        if not s[p1].isalnum():
+            p1=p1+1
+        elif not s[p2].isalnum():
+            p2=p2-1
+        elif s[p1].lower()!= s[p2].lower():
+            return False
+        else:
+            p1=p1+1
+            p2=p2-1
+    return True
+
+print(valid_plaindrome("A man, a plan, a canal: Panama"))
