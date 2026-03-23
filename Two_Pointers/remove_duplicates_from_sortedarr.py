02b9fc91d6a09581017fcca189c063b61bbd9534
thanmai <thanmaireddy77@gmail.com>
2026-03-23 22:27:39 +0530

    remove duplicates from sorting array- the sorted advantage

--- Two_Pointers/remove_duplicates_from_sortedarr.py
+++ Two_Pointers/remove_duplicates_from_sortedarr.py
@@ -1,2 +1,14 @@
-result= [1,2,3,4]
-print(result**2)
+# nums= [1,1,2,2,3,3,3,5]
+# output= [1,2,3,5,_,_,_,_]
+def remove_duplicates_from_sorted_array(nums):
+    n= len(nums)
+    p1=1
+    for i in range(1,n):
+        if nums[p1-1]!= nums[i]:
+            nums[p1]= nums[i]
+            p1=p1+1
+    return p1
+
+print(remove_duplicates_from_sorted_array([1,1,2,3,3,4,4,6]))
+        
+        
