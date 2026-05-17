a1842773d0d84fe896781146830e3bc02e77c9eb
thanmai <thanmaireddy77@gmail.com>
2026-05-17 12:49:08 +0530

    two pointers- intermediate problems: squares of a sorted array

--- Two_Pointers/squares_of_a_sorted_arr.py
+++ Two_Pointers/squares_of_a_sorted_arr.py
@@ -1,21 +1,19 @@
 def squares_of_a_sorted_array(nums):
     n= len(nums)
     p1=0
-    p2=n-1
-    result=[0]*n
-    tobereplaced = n-1
+    p2= n-1
+    result= [0]*n
+    tobereplaced= n-1
     while (p1<=p2):
         if abs(nums[p1])< abs(nums[p2]):
-            result[tobereplaced]= nums[p2]
-            tobereplaced-=1
+            result[tobereplaced]= nums[p2]**2
             p2=p2-1
-        else:
-            result[tobereplaced]= nums[p1]
             tobereplaced-=1
+        else:
+            result[tobereplaced]= nums[p1]**2
             p1=p1+1
-    result1=[]
-    for num in result:
-        result1.append(num**2)
-    return result1
+            tobereplaced-=1
+    return result 
 
-print(squares_of_a_sorted_array( [-4,-1,0,3,10]))
+print(squares_of_a_sorted_array([-7,-3,2,3,11]))
+            
