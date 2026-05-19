400c684b22d12aa12eba96db7c82881fd65df103
thanmai <thanmaireddy77@gmail.com>
2026-05-19 23:04:06 +0530

    LC daily challenge problem

---
+++ Daily_Challenge/minimum_common_value.py
@@ -0,0 +1,16 @@
+def minimum_common_value(nums1, nums2):
+    n= len(nums1)
+    m= len(nums2)
+    p1=0
+    p2=0
+    while (p1<n and p2<m):
+        if nums1[p1]< nums2[p2]:
+            p1=p1+1
+        elif nums1[p1]> nums2[p2]:
+            p2=p2-1
+        else:
+            return nums1[p1]
+        
+    return -1
+
+print(minimum_common_value([1,2,3,6],[2,3,4,5]))
