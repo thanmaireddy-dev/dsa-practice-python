5405c2d29043b91df0aa3f07c22396edc5565fe8
thanmai <thanmaireddy77@gmail.com>
2026-05-20 07:26:16 +0530

    Valid palindrome 2- two pointers with a helper function

---
+++ Two_Pointers/valid_palindrome_II.py
@@ -0,0 +1,25 @@
+def valid_palindrome_two(s):
+    n= len(s)
+    p1=0
+    p2=n-1
+    
+    def is_palindrome(s, p1, p2): #helper funtion to determine if palindrome or not
+        while (p1<p2):
+            if s[p1]==s[p2]:
+                p1=p1+1
+                p2=p2-1
+            else:
+                return False
+        return True
+    
+    while(p1<p2):
+        if s[p1]==s[p2]:
+            p1=p1+1
+            p2=p2-1
+        else:
+            return is_palindrome(s,p1+1,p2) or is_palindrome(s,p1,p2-1)
+    return True
+
+
+print(valid_palindrome_two("abd"))
+                
