74e7371f0c33af4b8c286fb29d5623fd920fc18f
thanmai <thanmaireddy77@gmail.com>
2026-05-20 18:23:47 +0530

    Pair with largest sum which is less than K in the array

--- Two_Pointers/2Sum_less_than_K_pair.py
+++ Two_Pointers/2Sum_less_than_K_pair.py
@@ -1,9 +1,10 @@
-def two_sum_less_than_k(nums, target):
+def two_sum_less_than_k_Pair(nums, target):
     n= len(nums)
     nums.sort()
     p1=0
     p2=n-1
     bestsum= -1
+    bestpair=[0,0]
     if n<2:
         return -1
     while (p1<p2):
@@ -11,12 +12,11 @@ def two_sum_less_than_k(nums, target):
         if currsum<target:
             if currsum> bestsum:
                 bestsum= currsum
+                bestpair= [nums[p1], nums[p2]]
             p1=p1+1
         else:
             p2=p2-1
-    return bestsum
+    return bestpair if bestpair!= [0,0] else [-1,-1]
 
 
-print(two_sum_less_than_k([2, 3, 4, 6, 8, 10], 10))
-    
-    
+print(two_sum_less_than_k_Pair([2, 3, 4, 6, 8, 10], 0))
