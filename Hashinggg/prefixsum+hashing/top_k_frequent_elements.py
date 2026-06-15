f667784a57f06875ea00ab5a5161d0299222bd9b
thanmai <thanmaireddy77@gmail.com>
2026-06-15 14:23:48 +0530

    Top K Frequent Elements

--- Hashinggg/prefixsum+hashing/top_k_frequent_elements.py
+++ Hashinggg/prefixsum+hashing/top_k_frequent_elements.py
@@ -9,15 +9,17 @@ def top_k_frequent_elements(nums,k):
             seen[num]=1
     for number in seen:
         freq= seen[number]
-        pair= [number, seen]
+        pair= [freq, number]
         freq_pairs.append(pair)
     freq_pairs.sort()
     
-    for num in reversed(freq_pairs[-k:]):
-        result.append(num[1])
+    for barbie in reversed(freq_pairs[-k:]):
+        result.append(barbie[1])
     return result
 
-print(top_k_frequent_elements([1,1,1,2,2,3], 2))
+print(top_k_frequent_elements([1,2,1,2,1,2,3,1,3,2], 2))
+    
+    
         
         
     
