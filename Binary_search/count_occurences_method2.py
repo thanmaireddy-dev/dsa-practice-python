0fd80cbbb0074d2fdad13ae3413d53b4a71400e9
thanmai <thanmaireddy77@gmail.com>
2026-06-21 11:17:45 +0530

    Count occurences in a sorted array- method 2- using find first and find last helper functions.

---
+++ Binary_search/count_occurences_method2.py
@@ -0,0 +1,37 @@
+def count_occurences_in_sorted_array_GFG(nums, target):
+    n= len(nums)
+    def find_first(nums, target):
+        low=0
+        high=n-1
+        while (low<=high):
+            mid=(low+high)//2
+            if nums[mid]==target:
+                first=mid
+                high=mid-1
+            elif nums[mid]<target:
+                low=mid+1
+            else:
+                high=mid-1
+        return first
+    def find_last(nums, target):
+        low=0
+        high=n-1
+        while (low<=high):
+            mid=(low+high)//2
+            if nums[mid]==target:
+                last=mid
+                low=mid+1
+            elif nums[mid]<target:
+                low=mid+1
+            else:
+                high=mid-1
+        return last
+    
+    res1=find_first(nums, target)
+    res2=find_last(nums, target)
+    return res2-res1+1
+
+print(count_occurences_in_sorted_array_GFG([1,1,2,2,2,2,3,4],1))
+        
+                
+    
