0fd44d31b2e8f59b04727d3754dd909dbadc8ad1
thanmai <thanmaireddy77@gmail.com>
2026-07-02 19:27:32 +0530

    Rewriting Sqrt(x) in a diff way

--- Binary_search/sqrt(x).py
+++ Binary_search/sqrt(x).py
@@ -1,16 +1,15 @@
 def sqrt(x):
     if x<2:
         return x
-    low=0
-    high=x-1
-    result=-1
+    low=1
+    high=x
     while (low<=high):
         mid=(low+high)//2
         if mid*mid==x:
             return mid
         elif mid*mid<x:
-            low=mid+1
             result=mid
+            low=mid+1
         else:
             high=mid-1
     return result
