e672a4bf2c03b4c7fb5931ced679fc8673de41a0
thanmai <thanmaireddy77@gmail.com>
2026-07-04 08:03:27 +0530

    Simplifying Buy and sell stock II

--- Resource_Optimization_Greedy/buy&sell_stock_II.py
+++ Resource_Optimization_Greedy/buy&sell_stock_II.py
@@ -1,12 +1,10 @@
 def best_time_to_buy_and_sell_stock_II(prices):
     n= len(prices)
     profit=0
-    max_profit= 0
-    for i in range(n-1):
-        if prices[i+1]> prices[i]:
-            profit= profit+ (prices[i+1]- prices[i])
-            max_profit= max(max_profit, profit)
-    return max_profit
+    for i in range(1,n):
+        if prices[i]> prices[i-1]:
+            profit= profit+ (prices[i]-prices[i-1])
+    return profit
 
-print(best_time_to_buy_and_sell_stock_II([7,1,5,3,6,4]))
+print(best_time_to_buy_and_sell_stock_II([1,2,3,4,5]))
     
