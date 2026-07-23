984fc1294939459a3d4cd6af96debf19c0ea96f8
thanmai <thanmaireddy77@gmail.com>
2026-07-23 07:44:11 +0530

    Subtree of Another Tree

---
+++ Trees/compare_transform_trees/subtree_of_another_tree.py
@@ -0,0 +1,24 @@
+lass Solution(object):
+    def isSubtree(self, root, subRoot):
+        def isIdentical(root,subRoot):
+            if root is None and subRoot is None:
+                return True
+            if root is None or subRoot is None:
+                return False
+            if root and subRoot and (root.val!=subRoot.val):
+                return False
+            return isIdentical(root.left, subRoot.left) and isIdentical(subRoot.right, root.right)
+            
+
+        stack=[]
+        stack.append(root)
+        while stack:
+            root= stack.pop()
+            if root.val==subRoot.val:
+                if isIdentical(root, subRoot):
+                    return True
+            if root.right:
+                stack.append(root.right)
+            if root.left:
+                stack.append(root.left)
+        return False
