d09ba7c18c5d5c17007b03ad74f8600b759bdd74
thanmai <thanmaireddy77@gmail.com>
2026-09-05 19:23:11 +0530

    daily challenge 05 sept

---
+++ Daily_Challenge/smallest_stable_index_II.py
@@ -0,0 +1,17 @@
+def firstStableIndex_II(nums, k):
+        n= len(nums)
+        leftmax=[0]*n
+        rightmin=[0]*n
+        leftmax[0]= nums[0]
+        rightmin[n-1]= nums[n-1]
+        for i in range(1,n):
+            leftmax[i]= max(leftmax[i-1],nums[i])
+        for i in range(n-2,-1,-1):
+            rightmin[i]= min(rightmin[i+1], nums[i])
+        for i in range(n):
+            stability= leftmax[i]-rightmin[i]
+            if stability<=k:
+                return i
+        return -1
+
+print(firstStableIndex_II([5,0,1,4],3))
