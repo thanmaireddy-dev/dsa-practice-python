031fe5b75fb7a4397a8cbe58d1b804a93a8c84ca
thanmai <thanmaireddy77@gmail.com>
2026-09-20 16:39:08 +0530

    Longest Common Prefix

---
+++ Strings/longest_common_prefix.py
@@ -0,0 +1,11 @@
+def longest_common_prefix(strs):
+    if not strs:
+        return ""
+    for i in range(len(strs[0])):
+        char= strs[0][i]
+        for word in strs[1:]:
+            if i>=len(word) or word[i]!=char:
+                return strs[0][:i]
+    return strs[0]
+
+print(longest_common_prefix(["cluster", "class", "clam", "clouds", "click"]))
