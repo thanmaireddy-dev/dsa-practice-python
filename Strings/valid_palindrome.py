8a769fb58edefb55d84b41a77ae900cf5e19e821
thanmai <thanmaireddy77@gmail.com>
2026-09-20 08:59:27 +0530

    Valid Palindrome

---
+++ Strings/valid_palindrome.py
@@ -0,0 +1,18 @@
+def valid_palindrome(s):
+    n= len(s)
+    p1=0
+    p2=n-1
+    while (p1<p2):
+        if not s[p1].isalnum():
+            p1=p1+1
+        elif not s[p2].isalnum():
+            p2=p2-1
+        elif s[p1].lower()==s[p2].lower():
+            p1=p1+1
+            p2=p2-1
+        else:
+            return False
+    return True
+
+print(valid_palindrome( "A man, a plan, a canal: Panama"))
+        
