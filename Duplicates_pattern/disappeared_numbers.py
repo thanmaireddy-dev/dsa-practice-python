d54c5d7575fc86b5f6fa80c3e8fc6bbd3083c62b
thanmai <thanmaireddy77@gmail.com>
2026-09-25 15:29:14 +0530

    Find All Numbers Disappeared in an Array

---
+++ Duplicates_pattern/disappeared_numbers.py
@@ -0,0 +1,11 @@
+def disappeared_numbers(nums):
+    for num in nums:
+        i= abs(num)-1
+        nums[i]= -1*abs(nums[i])
+    result=[]
+    for i,num in enumerate(nums):
+        if num>0:
+            result.append(i+1)
+    return result
+
+print(disappeared_numbers([4,3,2,7,8,2,3,1]))
